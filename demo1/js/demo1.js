411c9a5735fbb1d295b0477b4240ac2ea3d3bbca
belgort-clark <belgort@clark.edu>
2020-05-04 15:04:24 -0700

    Code from class

--- demo1/js/demo1.js
+++ demo1/js/demo1.js
@@ -1,6 +1,47 @@
 // demo1.js
 
 // define constants
+const addToEnd = document.querySelector('#add')
+const addToTop = document.querySelector('#top')
+const deleteBottom = document.querySelector('#deletebottom')
+const deleteTop = document.querySelector('#deletetop')
+// const addToEnd = document.getElementById('add')
+const ul = document.querySelector('#stuff')
+let count = 1
+let countT = 1
 
 // create eventlisteners - one for each button
+addToEnd.addEventListener('click', function () {
+    const li = document.createElement('li')
+    li.innerText = `Hello I am number ${count}`
+    ul.appendChild(li)
+    count++
+    deleteBottom.style.display = 'inline'
+})
 
+addToTop.addEventListener('click', function () {
+    const listItems = document.querySelectorAll('#stuff li')
+    const li = document.createElement('li')
+    li.innerText = `I am on the top of the list! ${countT}`
+    ul.insertBefore(li, listItems[0])
+    countT++
+    deleteBottom.style.display = 'inline'
+})
+
+deleteBottom.addEventListener('click', function () {
+    let listItems = document.querySelectorAll('#stuff li')
+    if (listItems.length >= 1) {
+        listItems[listItems.length - 1].remove()
+        listItems = document.querySelectorAll('#stuff li')
+        if (listItems.length == 0) {
+            deleteBottom.style.display = 'none'
+        }
+    }
+})
+
+deleteTop.addEventListener('click', function () {
+    const listItems = document.querySelectorAll('#stuff li')
+    if (listItems.length >= 1) {
+        listItems[0].remove()
+    }
+})
--- demo2/js/demo2.js
+++ demo2/js/demo2.js
@@ -1,5 +1,20 @@
 // demo2.js
 
 // get all pics using attribute selectors
+const pics = document.querySelectorAll('img[src$=".jpg"]')
+// console.log(pics)
+pics.forEach(function (element) {
+    element.setAttribute('height', '50')
+    element.setAttribute('alt', 'A pretty picture')
+    const span = document.createElement('span')
+    span.innerText = 'I like spam'
+    element.before(span)
+    element.addEventListener('click', function () {
+        // alert('you click me')
+        element.style.background = 'pink'
+        // element.setAttribute('src', 'images/100-400x400.jpg')
+        // element.remove()
+    })
+})
 
 // iterate through each element in the array and change attributes
