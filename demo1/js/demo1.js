1d8b9455c25d1155200596cebd88899fbdcdc1a7
Bruce Elgort <belgort@clark.edu>
2023-04-23 14:29:28 -0700

    Minor updates

--- demo1/js/demo1.js
+++ demo1/js/demo1.js
@@ -1,47 +1,53 @@
 // demo1.js
 
 // define constants
-const addToEnd = document.querySelector('#add')
-const addToTop = document.querySelector('#top')
-const deleteBottom = document.querySelector('#deletebottom')
-const deleteTop = document.querySelector('#deletetop')
+const addToEnd = document.querySelector('#add');
+const addToTop = document.querySelector('#top');
+const deleteBottom = document.querySelector('#deletebottom');
+const deleteTop = document.querySelector('#deletetop');
+
 // const addToEnd = document.getElementById('add')
-const ul = document.querySelector('#stuff')
-let count = 1
-let countT = 1
+const ul = document.querySelector('#stuff');
+let count = 1;
+let countT = 1;
 
 // create eventlisteners - one for each button
+
+// add to bottom button
 addToEnd.addEventListener('click', function () {
-    const li = document.createElement('li')
-    li.innerText = `Hello I am number ${count}`
-    ul.appendChild(li)
-    count++
-    deleteBottom.style.display = 'inline'
-})
+    const li = document.createElement('li');
+    li.innerText = `Hello I am number ${count}`;
+    ul.appendChild(li);
+    count++;
+    deleteBottom.style.display = 'inline';
+});
 
+// add to the top button
 addToTop.addEventListener('click', function () {
-    const listItems = document.querySelectorAll('#stuff li')
-    const li = document.createElement('li')
-    li.innerText = `I am on the top of the list! ${countT}`
-    ul.insertBefore(li, listItems[0])
-    countT++
-    deleteBottom.style.display = 'inline'
-})
+    const listItems = document.querySelectorAll('#stuff li');
+    const li = document.createElement('li');
+    li.innerText = `I am on the top of the list! ${countT}`;
+    ul.insertBefore(li, listItems[0]);
+    countT++;
+    deleteBottom.style.display = 'inline';
+});
 
+// delete from bottom button
 deleteBottom.addEventListener('click', function () {
-    let listItems = document.querySelectorAll('#stuff li')
+    let listItems = document.querySelectorAll('#stuff li');
     if (listItems.length >= 1) {
-        listItems[listItems.length - 1].remove()
-        listItems = document.querySelectorAll('#stuff li')
+        listItems[listItems.length - 1].remove();
+        listItems = document.querySelectorAll('#stuff li');
         if (listItems.length == 0) {
-            deleteBottom.style.display = 'none'
+            deleteBottom.style.display = 'none';
         }
     }
-})
+});
 
+// delete from the top button
 deleteTop.addEventListener('click', function () {
-    const listItems = document.querySelectorAll('#stuff li')
+    const listItems = document.querySelectorAll('#stuff li');
     if (listItems.length >= 1) {
-        listItems[0].remove()
+        listItems[0].remove();
     }
-})
+});
--- demo2/js/demo2.js
+++ demo2/js/demo2.js
@@ -1,23 +1,23 @@
 // demo2.js
 
 // get all pics using attribute selectors
-const pics = document.querySelectorAll('img[src$=".jpg"]')
+const pics = document.querySelectorAll('img[src$=".jpg"]');
 // console.log(pics)
-let i = 0
+let i = 0;
 pics.forEach(function (element) {
-    element.setAttribute('id', i)
-    i++
-    element.setAttribute('height', '50')
-    element.setAttribute('alt', 'A pretty picture')
-    const span = document.createElement('span')
-    span.innerText = 'I like spam'
-    element.before(span)
+    element.setAttribute('id', i);
+    i++;
+    element.setAttribute('height', '50');
+    element.setAttribute('alt', 'A pretty picture');
+    const span = document.createElement('span');
+    span.innerText = 'I like spam';
+    element.before(span);
     element.addEventListener('click', function () {
         // alert('you click me')
-        element.style.background = 'pink'
+        // element.style.background = 'pink';
         // element.setAttribute('src', 'images/100-400x400.jpg')
         // element.remove()
-        alert(element.getAttribute('id'))
+        // alert(element.getAttribute('id'));
     })
 })
 
